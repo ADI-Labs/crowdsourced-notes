b43c7b4e33b722a102fd1adbbcb16af8b123a40b
Howon <howonbyun@gmail.com>
2016-02-26 16:31:56 -0500

    updated config and readme

--- config/config.js
+++ config/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    port: process.env.PORT || 3000,
-    connectionString: 'mongodb://user:useruser@ds017688.mlab.com:17688/crowdsourced-notes'
+  app: {
+    port: process.env.PORT || 3000
+  },
+  mongo: {
+    url: 'mongodb://user:useruser@ds017688.mlab.com:17688/crowdsourced-notes'
+  }
 };
