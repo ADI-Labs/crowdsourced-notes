b9d213a7e2714b902c82833d8fe1bb0ccb18c10c
Robbie Netzorg <robbizorg@gmail.com>
2016-03-25 16:20:40 -0400

    Added Image Upload

--- public/javascript/socket.js
+++ public/javascript/socket.js
@@ -1,7 +1,58 @@
+var filesUpload = null;
+var file = null;
 var socket = io.connect('http://localhost:3000');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+var send = false;
 
-socket.emit('create', {post: {title: 'Something', content: 'Notes Notes Notes'}});
+
+if (window.File && window.FileReader && window.FileList) {
+    //HTML5 File API ready
+    init();
+} else {
+    //browser has no support for HTML5 File API
+    //send a error message or something like that
+    //TODO
+}
+
+/**
+ * Initialize the listeners and send the file if have.
+ */
+function init() {
+	window.onload = function() {
+		console.log(document.getElementById('title'));
+    	filesUpload = document.getElementById('input-files');
+    	filesUpload.addEventListener('change', fileHandler, false);		
+	}
+
+}
+
+/**
+ * Handle the file change event to send it content.
+ * @param e
+ */
+function fileHandler(e) {
+    var files = e.target.files || e.dataTransfer.files;
+
+    if (files) {
+        //send only the first one
+        file = files[0];
+    }
+}
+
+function sendFile() {
+    if (file) {
+        //read the file content and prepare to send it
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            console.log('Sending file...');
+            //get all content
+            var buffer = e.target.result;
+            //send the content via socket
+            socket.emit('create', {post: {title: 'Example', content: file.name, isImage: true}}, buffer);
+        };
+        reader.readAsBinaryString(file);
+    }
+}
+
+
+//socket.emit('create', {post: {title: 'Something', content: 'Notes Notes Notes'}});
--- scripts/socket/socket.js
+++ scripts/socket/socket.js
@@ -2,6 +2,8 @@
 var socketio = require('socket.io')
 var User = require('../models/Users');
 var Post = require('../models/Posts');
+var fs = require('fs');
+var path = require('path');
 
 module.exports.listen = function(app){
     io = socketio.listen(app)
@@ -14,12 +16,51 @@ module.exports.listen = function(app){
     		io.emit('chat message', data.msg, data.user);
   		});
 
-  		socket.on('create', function(data) {
+  		socket.on('create', function(data, buffer) {
+        function genKey() {
+          function s4() {
+            var temp = (Math.floor(Math.random()*20));
+            console.log("TEMP: " + temp);
+            return temp;
+          }
+          return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4()
+           + '-' + s4() + s4() + s4();
+        }
+
+        var newPost = new Post();
+
+        if (data.post.isImage == true) {
+          var imageID = genKey();
+          var desiredPath = path.resolve('../lib/images/' + imageID);
+          console.log(data.post.content);
+
+          var fileName = path.resolve(__dirname, '../../lib/images/' + imageID);
+          console.log(fileName);
+
+          fs.open(fileName, 'a', 0755, function(err, fd) {
+            if (err) throw err;
+
+            fs.write(fd, buffer, null, 'Binary', function(err, written, buf) {
+              fs.close(fd, function() {
+                console.log("File saved successful!");
+              })
+            })
+          })
+          
+          newPost.content = path.resolve(__dirname, '../../lib/images/' + imageID);
+
+        } else {
+          newPost.content = data.post.content;
+        }
   			console.log("Creating " + data.post.title);
 
-  			var newPost = new Post();
+  			
   			newPost.title = data.post.title;
-  			newPost.content = data.post.content;
+        newPost.date = data.post.date;
+        newPost.postedOn = new Date();
+        newPost.upvotes = 0;
+        newPost.comments = [];
+        newPost.tags = data.post.tags;
 
 			newPost.save(function(err) {
 				if (err)
