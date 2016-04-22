a7f009e1d8a4282b73ea9f45b957a721db53cebf
Leon Song <ls3233@columbia.edu>
2016-04-22 16:26:46 -0400

    Finished sectionview

--- public/javascript/views/SectionView.js
+++ public/javascript/views/SectionView.js
@@ -1,28 +1,35 @@
 define(['jquery',
 	'underscore',
 	'backbone',
+	'models/LectureModel',
 	'stache!/templates/section'],
-	function ($, _, Backbone, template) {
+	function ($, _, Backbone, LectureModel, template) {
 		return Backbone.View.extend({
 			initialize: function () {
-				this.listenTo(this.model, 'sync change', this.render);
+				this.listenTo(this.model, 'sync change', this.mostRecent);
 				this.model.fetch();
-				this.render();
 			},
 			render: function () {
-				console.log('section', this.model);
+				this.$el.html(template({
+					users: this.model.toJSON(),
+					section: this.model.toJSON(),
+					mostRecentLecture: this.mostRecentLecture.toJSON()
+				}));
+			},
+			mostRecent: function () {
 				var recentLecture;
 				_.each(this.model.attributes.lectures, function (lecture, i) {
 					recentLecture = i == 0 ? lecture : recentLecture;
 					if (new Date(lecture.date) > new Date(recentLecture.date)) {
 						recentLecture = lecture;
 					} 
-				})
-				this.$el.html(template({
-					users: this.model.toJSON(),
-					section: this.model.toJSON(),
-					mostRecentLecture: recentLecture
-				}));
+				});
+				this.mostRecentLecture = new LectureModel({
+					_id: recentLecture._id
+				});
+				this.mostRecentLecture.fetch({
+					success:this.render.bind(this)
+				});
 			}
 		});
 	}
