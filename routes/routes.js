9f7128860ba6a663728668945e3bc410316e00b2
Howon <howonbyun@gmail.com>
2016-04-22 13:16:51 -0400

    routing action

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,29 @@
 module.exports = function(app, passport) {
-	app.get('/', function(req, res) {
-		res.render('index', {
-			title: "Hello World"
-		})
-	});
+		app.get('/', function(req, res) {
+			// if (req.isAuthenticated()) {
+				res.redirect('/#/dashboard');
+			// } else {
+				res.render('index', {
+					title: "Hello World"
+				})
+			// }
+		});
 
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+		app.get('/logout', function(req, res) {
+			req.logout();
+			res.redirect('/');
+		});
 
-	// Google Routes
-	app.get('/auth/google', passport.authenticate('google', {
-		scope: ['profile', 'email']
-	}));
+		// Google Routes
+		app.get('/auth/google', passport.authenticate('google', {
+			scope: ['profile', 'email']
+		}));
 
-	// google callback
-	app.get('/auth/google/callback',
-		passport.authenticate('google', {
-			successRedirect: '/#/dashboard',
-			failureRedirect: '/'
-		})
-	);
-}
+		// google callback
+		app.get('/auth/google/callback',
+			passport.authenticate('google', {
+				successRedirect: '/#/dashboard',
+				failureRedirect: '/'
+			})
+		);
+	}
